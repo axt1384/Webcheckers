c835cae100d6ca79c53159384d0c68f262283291
cante <cantellaelijah@gmail.com>
2017-10-14 15:13:30 -0400

    Implemented GetSignInRoute. The handle() method needs re-implemented. This file is based directly off of GetHomeRoute.

---
+++ src/main/java/com/webcheckers/ui/GetSignInRoute.java
@@ -0,0 +1,70 @@
+package com.webcheckers.ui;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.TemplateEngine;
+
+/**
+ * The UI Controller to GET the Sign In page.
+ *
+ * @author <a href='mailto:edc8230@rit.edu'>Elijah Cantella</a>
+ */
+public class GetSignInRoute implements Route {
+
+    // ----------
+    // Attributes
+    // ----------
+    private static final Logger LOG = Logger.getLogger(GetHomeRoute.class.getName());
+
+    private final TemplateEngine templateEngine;
+
+    // ------------
+    // Constructors
+    // ------------
+
+    /**
+     * Create the Spark Route (UI controller) for the
+     * {@code GET /} HTTP request.
+     *
+     * @param templateEngine
+     *   the HTML template rendering engine
+     * @param templateEngine
+     */
+    public GetSignInRoute(final TemplateEngine templateEngine) {
+        // validation
+        Objects.requireNonNull(templateEngine, "templateEngine must not be null");
+
+        this.templateEngine = templateEngine;
+        LOG.config("GetSignInRoute is initialized.");
+    }
+
+    // -------
+    // Methods
+    // -------
+
+    /**
+     * Render the WebCheckers Home page.
+     *
+     * @param request
+     *   the HTTP request
+     * @param response
+     *   the HTTP response
+     * @return
+     *   the rendered HTML for the Home page.
+     */
+    @Override
+    public Object handle(Request request, Response response) {
+        LOG.finer("GetSignInRoute is invoked.");
+
+        Map<String, Object> vm = new HashMap<>();
+        vm.put("title", "Welcome!"); // What is this?
+        return templateEngine.render(new ModelAndView(vm , "signin.ftl"));
+    }
+}
