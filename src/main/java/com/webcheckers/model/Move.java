6f533e04d200a327da4b4c426715d56afb31be75
jay gogri <jkg6656@g.rit.edu>
2017-11-06 19:32:42 -0500

    edited Move file to make move

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -11,11 +11,30 @@ public class Move {
     // Attributes
     // ----------
 
+    Row fromRow;
+    int fromCol;
+    Row toRow;
+    int toCol;
+
     // ------------
     // Constructors
     // ------------
 
+    public Move(Row r1, int c1, Row r2, int c2) {
+        // Constructor.  Just set the values of the instance variables.
+        fromRow = r1;
+        fromCol = c1;
+        toRow = r2;
+        toCol = c2;
+    }
+
     // -------
     // Methods
     // -------
+    public boolean isJump() {
+        // Test whether this move is a jump.  It is assumed that
+        // the move is legal.  In a jump, the piece moves two
+        // rows.  (In a regular move, it only moves one row.)
+            return (fromRow.getIndex() - toRow.getIndex() == 2 || fromRow.getIndex() - toRow.getIndex() == -2);
+        }
 }
